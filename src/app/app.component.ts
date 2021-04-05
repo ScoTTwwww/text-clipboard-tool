5a6f0d49456000ad1cbf6235e6cbb2b1b25d1784
scott <scott21739@gmail.com>
2021-04-05 22:29:26 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ export class AppComponent {
     },
   ];
 
-  textList: Array<string>;
+  currentIndex: number = 0;
+  textList: Array<{ title: string, content: any }>;
 
   constructor(
     private http: HttpClient,
@@ -28,14 +29,44 @@ export class AppComponent {
 
     this.http.get('assets/text.txt', { responseType: 'text' })
       .subscribe(data => {
+        const list = data.split('=====================item=======================');
+        const dataList = [];
+        list.forEach((item: any) => {
 
-        this.textList = data.split('=====================item=======================');
-         console.log(data.split('=====================item======================='))
+          if (item.split('--------').length > 2) {
+            const title = item.split('--------')[1];
+            const content = item.split('--------')[2];
+            const lineLength = content.split("\n").length;
+
+            dataList.push({
+              title: title,
+              content: content,
+              lineLength: lineLength
+            })
+          } else {
+            dataList.push({
+              title: '',
+              content: item,
+              lineLength: item.split("\n").length
+            })
+          }
+        })
+
+        this.textList = dataList;
+        console.log(dataList)
       });
   }
 
 
-  clickText() {
+  clickText(text, index) {
+    this.currentIndex = index;
+    document.addEventListener('copy', (e: ClipboardEvent) => {
+      e.clipboardData.setData('text/plain', (text));
+      e.preventDefault();
+      document.removeEventListener('copy', null);
+    });
+    document.execCommand('copy');
+
     this.showToast('bottom-end', 'success', `更新成功 !`);
   }
 
